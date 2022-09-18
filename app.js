9d7f34414704be00fe4bab6a2bb9fba65292a21c
Talha Aman <talha.aman.bajwa@gmail.com>
2022-09-18 21:39:55 +0500

    filter method of array methods practice added

--- app.js
+++ app.js
@@ -1,23 +1,48 @@
-// for form submit
-const form = document.querySelector('.signup-form');
-const feedback = document.querySelector('.feedback');
-const usernameValidation = /^[a-zA-Z]{6,12}$/;
+// // for form submit
+// const form = document.querySelector('.signup-form');
+// const feedback = document.querySelector('.feedback');
+// const usernameValidation = /^[a-zA-Z]{6,12}$/;
 
-form.addEventListener('submit', e => {
-  e.preventDefault();
-  // console.log(form.username.value);
-  const username = form.username.value;
+// form.addEventListener('submit', e => {
+//   e.preventDefault();
+//   // console.log(form.username.value);
+//   const username = form.username.value;
 
-  if(usernameValidation.test(username)){
-    feedback.textContent = 'Congrats, Username is valid.'
-  } else {
-    feedback.textContent = 'Sorry, Incorrect username. Username must only contains alphabet ranges between 6 to 12 characters long';
-  }
-})
- form.addEventListener('keyup', e => {
-  if(usernameValidation.test(e.target.value)){
-    form.username.setAttribute('class', 'success');
-  } else {
-    form.username.setAttribute('class', 'error');
-  }
- });
+//   if(usernameValidation.test(username)){
+//     feedback.textContent = 'Congrats, Username is valid.'
+//   } else {
+//     feedback.textContent = 'Sorry, Incorrect username. Username must only contains alphabet ranges between 6 to 12 characters long';
+//   }
+// })
+//  form.addEventListener('keyup', e => {
+//   if(usernameValidation.test(e.target.value)){
+//     form.username.setAttribute('class', 'success');
+//   } else {
+//     form.username.setAttribute('class', 'error');
+//   }
+//  });
+
+
+
+//Filter Method --> Array Methods
+const scores = [10,20,25,30,35,50,60];
+const filteredScores = scores.filter(score => {
+  return score > 20;
+});
+console.log(filteredScores);
+
+const users = [
+  {name: 'sameer', pass: true},
+  {name: 'huzi', pass: true},
+  {name: 'pk', pass: false},
+  {name: 'asnan', pass: true},
+  {name: 'athar', pass: false},
+  {name: 'sohaib', pass: true},
+  {name: 'tashi', pass: false},
+  {name: 'zahid', pass: true},
+
+];
+const passedUsers = users.filter(user => {
+  return user.pass;
+});
+console.log(passedUsers);

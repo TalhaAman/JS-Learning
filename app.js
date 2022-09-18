7123dba6d9b7b8a96f3190018de31052e1683b5c
Talha Aman <talha.aman.bajwa@gmail.com>
2022-09-18 15:08:26 +0500

    Added form validation using REGEX technique

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
-//for form submit
+// for form submit
 const form = document.querySelector('.signup-form');
+const feedback = document.querySelector('.feedback');
 form.addEventListener('submit', e => {
   e.preventDefault();
-  console.log(form.username.value);
+  // console.log(form.username.value);
+  const username = form.username.value;
+  const usernameValidation = /^[a-zA-Z]{6,12}$/;
+  if(usernameValidation.test(username)){
+    feedback.textContent = 'Congrats, Username is valid.'
+  } else {
+    feedback.textContent = 'Sorry, Incorrect username. Username must only contains alphabet ranges between 6 to 12 characters long';
+  }
 })

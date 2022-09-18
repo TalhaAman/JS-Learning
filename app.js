15f7ce9073f8fb211be1c806e1ca8af85ea4c36a
Talha Aman <talha.aman.bajwa@gmail.com>
2022-09-18 22:45:29 +0500

    map method of array methods added

--- app.js
+++ app.js
@@ -25,24 +25,54 @@
 
 
 //Filter Method --> Array Methods
-const scores = [10,20,25,30,35,50,60];
-const filteredScores = scores.filter(score => {
-  return score > 20;
-});
-console.log(filteredScores);
+// const scores = [10,20,25,30,35,50,60];
+// const filteredScores = scores.filter(score => {
+//   return score > 20;
+// });
+// console.log(filteredScores);
 
-const users = [
-  {name: 'sameer', pass: true},
-  {name: 'huzi', pass: true},
-  {name: 'pk', pass: false},
-  {name: 'asnan', pass: true},
-  {name: 'athar', pass: false},
-  {name: 'sohaib', pass: true},
-  {name: 'tashi', pass: false},
-  {name: 'zahid', pass: true},
+// const users = [
+//   {name: 'sameer', pass: true},
+//   {name: 'huzi', pass: true},
+//   {name: 'pk', pass: false},
+//   {name: 'asnan', pass: true},
+//   {name: 'athar', pass: false},
+//   {name: 'sohaib', pass: true},
+//   {name: 'tashi', pass: false},
+//   {name: 'zahid', pass: true}
 
+// ];
+// const passedUsers = users.filter(user => {
+//   return user.pass;
+// });
+// console.log(passedUsers);
+
+
+
+// Map method of Array methods
+const prices = [10,20,25,30,35,50,60];
+const salePrices = prices.map(price => {
+  if(price > 30){
+    return price/2;
+  } else {
+    return price;
+  }
+})
+console.log(salePrices);
+
+const products = [
+  {name: 'soap', price: 80},
+  {name: 'shampoo', price: 150},
+  {name: 'buscuits', price: 60},
+  {name: 'vegetables', price: 120},
+  {name: 'fruits', price: 250}
 ];
-const passedUsers = users.filter(user => {
-  return user.pass;
-});
-console.log(passedUsers);
+const saleProducts = products.map(product => {
+  if(product.price > 100){
+    // product.price = product.price / 2; //This is destructive so we are not using this 
+    return {name: product.name, price: product.price/2}
+  } else {
+    return product;
+  }
+})
+console.log(saleProducts);
